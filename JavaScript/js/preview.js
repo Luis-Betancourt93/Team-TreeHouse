fc59b6153e001e3a4d740ad58e11f1d300c8a032
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-09-15 21:43:23 -0700

    Arrow Functions Practice

--- JavaScript/js/preview.js
+++ JavaScript/js/preview.js
@@ -2,20 +2,64 @@
 // ---------------- Functions -----------
 
 // Creating a Function
-function consoleRandom () {
-  const randomNumber = Math.floor( Math.random() * 6 ) + 1;
-  console.log(randomNumber);
-}
+// function consoleRandom () {
+//   const randomNumber = Math.floor( Math.random() * 6 ) + 1;
+//   console.log(randomNumber);
+// }
 
 // Calling a function
-consoleRandom();
+// consoleRandom();
 
 
 // Passing multiple arguments 
 
-function getArea(width, length) {
- const area = width * length;
- return area;
-}
+// function getArea(width, length) {
+//  const area = width * length;
+//  return area;
+// }
  
-console.log(getArea(20, 20));
+// console.log(getArea(20, 20));
+
+
+
+//  ARROW FUNCTION SYNTAX / They are not hosted
+//  const square = (x) => {
+//   return x * x;
+//  };
+
+//  Change this function to an arrow function
+// function getRandomNumber() {
+//   const randomNumber = Math.floor(Math.random() * 6 ) + 1;
+//   return randomNumber;
+// }
+
+// Arrow Function
+// const getRandomNumber = () => {
+//   const randomNumber = Math.floor(Math.random() * 6 ) + 1;
+//   return randomNumber;
+// };
+
+//  Change this function to an arrow function
+// function getArea(width, length, unit) {
+//   const area = width * length;
+//   return `${area} ${unit}`
+// }
+
+// const getArea = (width, length, unit) => {
+//   const area = width * length;
+//   return `${area} ${unit}`;
+// };
+
+// const getRandomNumbers = (higher, lower) => {
+//   let randomNumber = Math.floor(Math.random() * (higher - lower)) + 1;
+//   return randomNumber;
+// };
+
+// console.log(getRandomNumbers(5,10))
+
+function getRandomNumbers(lower, higher) {
+  let randomNumber = Math.floor(Math.random() * (higher - lower + 1 )) + lower;
+  return randomNumber;
+}
+
+console.log(getRandomNumbers(5, 10));
